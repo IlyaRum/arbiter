e14057d81a40c7cd019fc150b6eae3cef1bb5dbe
IlyaRum <tekstir@yandex.ru>
2025-09-02 12:20:09 +0300

    Ошибка подключения к wss://ia-oc-w-aiptst.cdu.so:433/api/public/core/v2.1/channels/open: Connection timed out: no further information: ia-oc-w-aiptst.cdu.so/172.16.82.96:433

--- src/main/java/arbiter/controller/WebSocketController.java
+++ src/main/java/arbiter/controller/WebSocketController.java
@@ -2,7 +2,10 @@ package arbiter.controller;
 
 import arbiter.config.AppConfig;
 import arbiter.service.WebSocketService;
+import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.WebSocketConnectOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -18,25 +21,30 @@ public class WebSocketController extends ABaseController{
 
   @Override
   public void registerRoutes(Router router) {
-    router.post(AppConfig.API_PREFIX + "/channels/connect").handler(this::connectToWebSocket);
+    router.get(AppConfig.API_PREFIX + "/channels/open").handler(this::connectToWebSocket);
   }
 
   public void connectToWebSocket(RoutingContext ctx) {
-    JsonObject requestBody = ctx.body().asJsonObject();
-    if (requestBody == null) {
-      handleError(ctx, new IllegalArgumentException("Request body is required"));
-      return;
-    }
+//    JsonObject requestBody = ctx.body().asJsonObject();
+//    if (requestBody == null) {
+//      handleError(ctx, new IllegalArgumentException("Request body is required"));
+//      return;
+//    }
 
-    String apiUrl = requestBody.getString("apiUrl");
-    String token = requestBody.getString("token");
+//    String apiUrl = requestBody.getString("apiUrl");
+//    String token = requestBody.getString("token");
+    String token = "ifxWuOiZjN00HLwgu20k9yYfYynvAkQOaO1YpcedliIvMJteTkHeiLibr-K99NPSwV7YONAav-iQSe_LcUzUFgHpuPXUTP-ozionoPmoWBJqJw68WYdt8ObcOhZYQnIrzKyu-HPP3DQr05CyiCgi_uK_soPoBp40C9fBbz08t4U4e_ikyTZioBg2UULaC-zdEv_MiTDToZ0P0iOZZmXQfL8OekaypTI9Wzu__TELHYFSpoIlabcDiAeYzie-iQRiElAaSfrbp3GGQt6ifk2ZgS9LxJ8dihm8Agc9E5EyKmFD6QSlcA4XvGiOi4WZ6pmlwLNia0W2sl9TA1Cw1Fy8WQ";
 
-    if (apiUrl == null || token == null) {
-      handleError(ctx, new IllegalArgumentException("apiUrl and token are required"));
-      return;
-    }
+//    if ( token == null) {
+//      handleError(ctx, new IllegalArgumentException("apiUrl and token are required"));
+//      return;
+//    }
 
-    webSocketService.connectToWebSocketServer(apiUrl, token)
+
+    Future<JsonObject> jsonObjectFuture = webSocketService.connectToWebSocketServer(token);
+
+
+    jsonObjectFuture
       .onSuccess(webSocket -> {
         JsonObject response = new JsonObject()
           .put("status", "connected")
@@ -49,4 +57,20 @@ public class WebSocketController extends ABaseController{
         handleError(ctx, throwable);
       });
   }
+
+//  private static void attemptReconnect(Vertx vertx) {
+//    if (retryCount < MAX_RETRIES) {
+//      retryCount++;
+//      long delay = (long) (1000 * Math.pow(2, retryCount)); // Экспоненциальная задержка
+//
+//      System.out.println("Reconnecting in " + delay + "ms (attempt " + retryCount + ")");
+//
+//      vertx.setTimer(delay, id -> {
+//        connectWebSocket(vertx);
+//      });
+//    } else {
+//      System.out.println("Max reconnection attempts reached");
+//      vertx.close();
+//    }
+//  }
 }
