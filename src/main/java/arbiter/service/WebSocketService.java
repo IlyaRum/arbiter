79635597a274afbe766c1948d0b96f147f023703
IlyaRum <tekstir@yandex.ru>
2025-12-24 16:58:21 +0300

    todo

--- src/main/java/arbiter/service/WebSocketService.java
+++ src/main/java/arbiter/service/WebSocketService.java
@@ -68,8 +68,9 @@ public class WebSocketService extends ABaseService {
     Promise<JsonObject> promise = Promise.promise();
 
     if (Objects.equals(AppConfig.getDevFlag(), "local")) {
+      //TODO[IER] Для разработки. Удалить.
       dependencyInjector.getHandleDataService().handleTextMessage(promise)
-        .handle(CloudEventStrings.MEASUREMENT_VALUES_DATA_V2_ONE_SECTION_PARAMETERS);
+        .handle(CloudEventStrings.MEASUREMENT_VALUES_DATA_V2);
     } else {
       WebSocketConnectOptions options = createWebSocketConnectOptions(token);
 
