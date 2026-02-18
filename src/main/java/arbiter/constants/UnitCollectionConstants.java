3f476cd45f45f4cc7a97e5049183ece643a00719
IlyaRum <tekstir@yandex.ru>
2026-02-18 15:36:39 +0300

    вынес в отдельный класс констант из UnitCollection

---
+++ src/main/java/arbiter/constants/UnitCollectionConstants.java
@@ -0,0 +1,31 @@
+package arbiter.constants;
+
+public class UnitCollectionConstants {
+
+  public static final String CONFIG_KEY_OIK = "ОИК";
+  public static final String CONFIG_KEY_ADDRESS = "адрес";
+  public static final String CONFIG_KEY_USER = "пользователь";
+  public static final String CONFIG_KEY_PASSWORD = "пароль";
+  public static final String CONFIG_KEY_DEBUG = "отладка";
+  public static final String CONFIG_KEY_WRITE_ENABLE = "запись в ОИК";
+  public static final String CONFIG_KEY_EVENT_UID = "изменение критерия МДП СМЗУ";
+  public static final String CONFIG_KEY_WRITE_EVENT_UID = "запись критерия МДП СМЗУ";
+  public static final String CONFIG_KEY_INSTANCE = "экземпляр";
+  public static final String CONFIG_KEY_SKIP_CYCLE = "не проверять данные при старте 2 цикла";
+  public static final String CONFIG_KEY_MINUS_HK = "вычитать НК";
+  public static final String CONFIG_KEY_EVENT_DELTA = "секунд между циклом расчета и критерием";
+  public static final String CONFIG_KEY_PORT = "порт";
+  public static final String CONFIG_KEY_REQUEST_DELAY = "интервал опроса данных";
+  public static final String CONFIG_KEY_CONNECT_ATTEMPT = "количество попыток соединения с ОИК";
+  public static final String CONFIG_KEY_OIK_CONNECT_TIMEOUT = "пауза перед соединением с ОИК";
+  public static final String CONFIG_KEY_WATCHDOG = "сторож";
+  public static final String CONFIG_KEY_HEARTBEAT_ID = "id";
+  public static final String CONFIG_KEY_HEARTBEAT_INTERVAL = "интервал";
+  public static final String CONFIG_KEY_WATCHDOG_WAIT = "ждать";
+  public static final String CONFIG_KEY_UNITS_ARRAY = "сечение";
+
+
+
+  private UnitCollectionConstants() {
+  }
+}
